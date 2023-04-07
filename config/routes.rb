960b48c730efc6c8318ffaefd431d347e68b4da7
claire greenwood <cg.clairegreenwood@gmail.com>
2023-04-07 15:41:20 +0100

    controller for comments generated, form for comments to be added and shown made

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
 
   resources :articles
+    resources :comments #creates comments as a nested resource within articles
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
 end
