3f472d14919ffc676876f7526d47c246cc99991e
claire greenwood <cg.clairegreenwood@gmail.com>
2023-04-07 09:17:08 +0100

    new and create methods added, private method added, validations added, link from index.html.erb file to new article created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
 
-  def show
-    @article = Article.find(params[:id])
+def index
+  @articles = Article.all
+end
+
+def show
+  @article = Article.find(params[:id])
+end
+
+def new
+  @article = Article.new
+end
+
+def create
+  @article = Article.new(article_params)
+
+  if @article.save 
+      redirect_to @article #if saved browser redirected to articles page
+  else
+      render :new, status: :unprocessable_entity #if article not saved form redisplayed and status code 422 displayed
+  end
+end
+
+private #filters params
+  def article_params
+      params.require(:article).permit(:title, :body)
   end
 end
