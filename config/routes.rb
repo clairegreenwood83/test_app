6a776792f55c2a7f505913ceb2c5bb63a56dec21
claire greenwood <cg.clairegreenwood@gmail.com>
2023-04-07 15:57:35 +0100

    code rendered using partial collections

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
 
-  resources :articles
+  resources :articles do
     resources :comments #creates comments as a nested resource within articles
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
+  end
 end
